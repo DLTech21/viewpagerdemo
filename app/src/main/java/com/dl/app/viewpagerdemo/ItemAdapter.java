317091c582e5600bdfc81995f122936b984d2f9f
tangyaopeng <donal.tong@gmail.com>
2016-12-07 08:45:16 +0800

    new

---
+++ app/src/main/java/com/dl/app/viewpagerdemo/ItemAdapter.java
@@ -0,0 +1,29 @@
+package com.dl.app.viewpagerdemo;
+
+import android.content.Context;
+import android.widget.TextView;
+
+
+import com.dl.app.viewpagerdemo.base.BaseRecyclerAdapter;
+import com.dl.app.viewpagerdemo.base.BaseRecyclerHolder;
+
+import java.util.List;
+
+/**
+ * @ explain:
+ * @ author：xujun on 2016/10/25 15:21
+ * @ email：gdutxiaoxu@163.com
+ */
+public class ItemAdapter extends BaseRecyclerAdapter<String> {
+
+    public ItemAdapter(Context context, List<String> datas) {
+        super(context, R.layout.item_string, datas);
+    }
+
+    @Override
+    public void convert(BaseRecyclerHolder holder, String item, int position) {
+        TextView tv=holder.getView(R.id.tv);
+        String s = mDatas.get(position);
+        tv.setText(s);
+    }
+}
---
+++ app/src/main/java/com/dl/app/viewpagerdemo/UI/CoordinatorLayoutActivity.java
@@ -0,0 +1,16 @@
+package com.dl.app.viewpagerdemo.UI;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.dl.app.viewpagerdemo.R;
+
+
+public class CoordinatorLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coordinator_layout);
+    }
+}
